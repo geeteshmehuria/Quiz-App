997135b67208b9461ba0ba8f27b821092d0914d2
geeteshmehuria <geeteshmaihuria@gmail.com>
2024-03-11 17:49:54 +0530

    question showing one by one

--- Quizapp/src/Pages/Quiz.jsx
+++ Quizapp/src/Pages/Quiz.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useCallback } from "react";
 import { Box, Button, Text } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 import axios from "axios";
-
+import "./css/quizCss.css";
 
 const Quiz = () => {
   const user = useSelector((state) => state.user);
@@ -10,6 +10,7 @@ const Quiz = () => {
   const [questionsData, setQuestionsData] = useState([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [isAnswer, setIsAnswer] = useState();
+  const [correctAns, setCorrectAns] = useState(0);
 
   useEffect(() => {
     if (user.category === "General Knowledge") {
@@ -47,8 +48,8 @@ const Quiz = () => {
 
   const handleCorrectAns = (option) => {
     if (option === questionsData[currentQuestionIndex].correct_answer) {
-      console.log("correct");
       setIsAnswer(true);
+      setCorrectAns(correctAns + 1);
     } else {
       console.log("incorrect");
       setIsAnswer(false);
@@ -57,47 +58,53 @@ const Quiz = () => {
   // console.log(questionsData[currentQuestionIndex].correct_answer);
   console.log(isAnswer);
   return (
-    <Box>
+    <Box
+      width={"90%"}
+      margin={"auto"}
+      mt={5}
+      display={"flex"}
+      flexDirection={"column"}
+      alignItems={"center"}
+    >
       {questionsData.length > 0 && (
         <>
-          <Text>{questionsData[currentQuestionIndex].question}</Text>
-          <Box display={"flex"} flexDirection={"column"} width={"20%"}>
+          <Text mb={2}>{questionsData[currentQuestionIndex].question}</Text>
+          <Box display={"flex"} flexDirection={"column"} width={"30%"}>
             <Button
               onClick={() =>
                 handleCorrectAns(
                   questionsData[currentQuestionIndex].correct_answer
                 )
               }
-             
             >
               {questionsData[currentQuestionIndex].correct_answer}
             </Button>
           </Box>
-          <Box display={"flex"} flexDirection={"column"} width={"20%"}>
+          <Box display={"flex"} flexDirection={"column"} width={"30%"}>
             {questionsData[currentQuestionIndex].incorrect_answers.map(
               (option, index) => (
-                <Button
-                  onClick={() => handleCorrectAns(option)}
-                  key={index}
-                  className={isAnswer ? "correct" : "incorrect"}
-                >
+                <Button onClick={() => handleCorrectAns(option)} key={index}>
                   {option}
                 </Button>
               )
             )}
           </Box>
-          <Button
-            onClick={handlePreviousQuestion}
-            disabled={currentQuestionIndex === 0}
-          >
-            Previous
-          </Button>
-          <Button
-            onClick={handleNextQuestion}
-            disabled={currentQuestionIndex === questionsData.length - 1}
-          >
-            Next
-          </Button>
+          <Box w={"30%"} display={"flex"} mt={2} n gap={2}>
+            <Button
+              w={"50%"}
+              onClick={handlePreviousQuestion}
+              disabled={currentQuestionIndex === 0}
+            >
+              Previous
+            </Button>
+            <Button
+              w={"50%"}
+              onClick={handleNextQuestion}
+              disabled={currentQuestionIndex === questionsData.length - 1}
+            >
+              Next
+            </Button>
+          </Box>
         </>
       )}
     </Box>
